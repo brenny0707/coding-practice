170923c1be888455cdc4ce73b5b718af51a1e37f
Brendan Ko <brenny0707@gmail.com>
2018-01-03 15:40:45 -0500

    Hook ReactDOM, import getbootstrap

--- max-react/src/app/index.js
+++ max-react/src/app/index.js
@@ -1,1 +1,18 @@
-console.log("HEYO");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sx-10 col-xs-offset-1">
+            <h1>Hello!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, window.document.getElementById("app"));
